5cc6face4240526b1112e23866b735c165ea482f
daleono@hotmail.com <daleono@hotmail.com>
2025-08-26 21:49:26 -0600

    Funcion mostrarAmigos

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-function creaArray(){
-    let amigos = [];
-    return amigos;
-}
-
 function agregarAmigo(){
     amigo = document.getElementById("amigo");
     if(amigo.value != ""){
         amigos.push(amigo.value);
         document.getElementById("amigo").value = "";
+        mostrarAmigos();
     }
     else
         alert("Por favor, inserte un nombre.");
-    console.log(amigos);
 }
 
-let amigos = creaArray();
+function mostrarAmigos(){
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    let mostrar = "";
+
+    for(let i = 0 ; i < amigos.length ; i++)
+        mostrar = `${mostrar} <li> ${amigos[i]} </li>`;
+
+    lista.innerHTML = mostrar;
+}
+
+var amigos = [];
